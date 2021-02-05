680fd0ed79fff21a47bb67086508ee8f89e1548b
mogaming <seiya.orz@gmail.com>
2021-02-05 12:50:00 +0900

    ADD: users

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get(':id')
+  get(@Param('id') id: string, @Query('q') q?: string): string {
+    return this.appService.getHello(id + q);
   }
 }
