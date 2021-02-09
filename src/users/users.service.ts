1694ef4518543e9e2198fefa723c7fc1250c4a5d
mogaming <seiya.orz@gmail.com>
2021-02-09 17:21:32 +0900

    prettier

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/lib/prisma/prisma.service';
+
 @Injectable()
 export class UsersService {
   constructor(private prisma: PrismaService) {}
